9cb463de0a34f697507d20c331afd61f29262e62
Siaticoski <caiosiaticoski1@gmail.com>
2024-11-08 13:45:40 -0400

    fff

--- projeto-front-end-2/src/app/layout.js
+++ projeto-front-end-2/src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Header from "@/components/Header";
 
 export const metadata = {
   title: "Star Rail",
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Header />
         {children}
       </body>
     </html>
--- projeto-front-end-2/src/components/Header/index.js
+++ projeto-front-end-2/src/components/Header/index.js
@@ -1,5 +1,19 @@
-const header = () => {
+import Link from "next/link"
+import styles from "./Header.module.css"
 
+export default function header (){
+    return(
+        <header>
+            <nav>
+                <ul className={styles.lista}>
+                    <li>
+                        <Link href='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/star_rail">star rail</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
-
-export default header
