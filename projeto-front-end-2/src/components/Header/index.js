7b61b456343e0c2207075d3ed19145bc2a46aefc
Siaticoski <caiosiaticoski1@gmail.com>
2024-11-08 14:29:13 -0400

    dsadas

--- projeto-front-end-2/src/components/Header/index.js
+++ projeto-front-end-2/src/components/Header/index.js
@@ -1,18 +1,30 @@
 import Link from "next/link"
 import styles from "./Header.module.css"
+import Image from "next/image"
 
 export default function header (){
     return(
-        <header>
-            <nav>
+        <header className={styles.cor}>
+            <nav className={styles.nav}>
+
                 <ul className={styles.lista}>
-                    <li>
-                        <Link href='/'>Home</Link>
+                    <li className={styles.links}>
+                        <Link className={styles.a} href='/'>Home</Link>
                     </li>
-                    <li>
-                        <Link href="/star_rail">star rail</Link>
+                    <li className={styles.links}>
+                        <Link className={styles.a} href="/star_rail">star rail</Link>
                     </li>
                 </ul>
+
+
+                <ul className={styles.img2}>
+                    <p>
+                        <Image src="/images/star_rail.jpg" alt="sim" width={80} height={80} />
+                    </p>
+                </ul>
+                
+                
+
             </nav>
         </header>
     )
