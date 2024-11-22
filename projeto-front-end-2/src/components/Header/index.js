e68a2f5d4bc01244cd17125344226fc3a1397836
Siaticoski <caiosiaticoski1@gmail.com>
2024-11-22 14:28:46 -0400

    dawdawdwad

--- projeto-front-end-2/src/components/Header/index.js
+++ projeto-front-end-2/src/components/Header/index.js
@@ -1,27 +1,34 @@
+"use client"
 import Link from "next/link"
 import styles from "./Header.module.css"
 import Image from "next/image"
+import { useState } from "react"
 
-export default function header (){
-    return(
+export default function header() {
+    const [links, setlinks] = useState(true);
+    return (
+
         <header className={styles.cor}>
             <nav className={styles.nav}>
-
                 <ul className={styles.lista}>
-                    <Image src="/images/" alt="sim" width={80} height={80} />
-                    <li className={styles.links}>
-                        <Link className={styles.a} href='/'>Home</Link>
-                    </li>
-                    <li className={styles.links}>
-                        <Link className={styles.a} href="/star_rail">Star Rail</Link>
-                    </li>
-                    <li>
-                        <Link className={styles.a} href="/reliquia">Relíquia</Link>
-                    </li>
-                </ul>
+                    <Image src="/images/pompom.png" alt="sim" width={60} height={60} />
 
-                
-                
+                    {links && (
+                        <ul className={styles.lista}>
+                            <li className={styles.links}>
+                                <Link className={styles.a} href='/'>Home</Link>
+                            </li>
+                            <li className={styles.links}>
+                                <Link className={styles.a} href="/star_rail">Star Rail</Link>
+                            </li>
+                            <li>
+                                <Link className={styles.a} href="/reliquia">Relíquia</Link>
+                            </li>
+                        </ul>
+                    )}
+
+
+                </ul>
 
             </nav>
         </header>
