24933ad6c163e9f517523cf97f90b963517f0881
Siaticoski <caiosiaticoski1@gmail.com>
2024-11-26 20:48:25 -0400

    adasdasd

--- projeto-front-end-2/src/components/Header/index.js
+++ projeto-front-end-2/src/components/Header/index.js
@@ -1,51 +1,32 @@
-"use client"
-import Link from "next/link"
-import styles from "./Header.module.css"
-import Image from "next/image"
-import { useState } from "react"
+"use client";
+import Link from "next/link";
+import styles from "./Header.module.css";
+import Image from "next/image";
+import { useState } from "react";
 
-export default function header() {     
+export default function Header() {     
+    const [links, setLinks] = useState(false);
 
-    const [links, setlinks] = useState(true);
-
-    
     return (
+        <header className={`${styles.cor} ${links ? styles.expanded : styles.collapsed}`}>
+            <button className={styles.butao} onClick={() => setLinks(!links)}>
+                <Image src="/images/menu.png" alt="sim" width={70} height={70} />
+            </button>
 
-        <header className={styles.cor}>
-
-        <button className={styles.butao} onClick={()=>setlinks(!links)}><Image src="/images/menu.png" alt="sim" width={70} height={70}/></button>
-
-            {links && (
-                <nav className={styles.nav}>
-
-                          
-
-                
-                    <ul className={styles.lista}>
-
-                        
-                        <Image className={styles.img} src="/images/pompom.png" alt="sim" width={60} height={60} />
-
-                        <li className={styles.links}>
-                            <Link className={styles.a} href='/'>Home</Link>
-                        </li>
-                        <li className={styles.links}>
-                            <Link className={styles.a} href="/star_rail">Star Rail</Link>
-                        </li>
-                        <li>
-                            <Link className={styles.a} href="/reliquia">Relíquia</Link>
-                        </li>
-                    </ul>
-                
-
-           
-
-        </nav>
-            )}
-            
-
+            <nav className={`${styles.nav} ${links ? styles.show : styles.hide}`}>
+                <ul className={styles.lista}>
+                    <Image className={styles.img} src="/images/pompom.png" alt="sim" width={60} height={60} />
+                    <li className={styles.links}>
+                        <Link className={styles.a} href='/'>Home</Link>
+                    </li>
+                    <li className={styles.links}>
+                        <Link className={styles.a} href="/star_rail">Star Rail</Link>
+                    </li>
+                    <li>
+                        <Link className={styles.a} href="/reliquia">Relíquia</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
-    )
-    
-    
+    );
 }
