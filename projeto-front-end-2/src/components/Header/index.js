7586cdaab39b8ad70d87b5d6591f5d370be7e9f6
Siaticoski <caiosiaticoski1@gmail.com>
2024-12-04 13:12:55 -0400

    j

--- projeto-front-end-2/src/components/Header/index.js
+++ projeto-front-end-2/src/components/Header/index.js
@@ -4,37 +4,29 @@ import styles from "./Header.module.css";
 import Image from "next/image";
 import { useState } from "react";
 
-export default function header() {
-    const [links, setlinks] = useState(true);
+export default function Header() {     
+    const [links, setLinks] = useState(false);
+
     return (
-        <header className={`${styles.cor} ${links ? styles.expanded : styles.collapsed}`}>
+       <header className={`${styles.cor} ${links ? styles.expanded : styles.collapsed}`}>
             <button className={styles.butao} onClick={() => setLinks(!links)}>
                 <Image src="/images/menu.png" alt="sim" width={70} height={70} />
             </button>
 
-        <header className={styles.cor}>
-            <nav className={styles.nav}>
+            <nav className={`${styles.nav} ${links ? styles.show : styles.hide}`}>
                 <ul className={styles.lista}>
-                    <Image src="/images/pompom.png" alt="sim" width={60} height={60} />
-
-                    {links && (
-                        <ul className={styles.lista}>
-                            <li className={styles.links}>
-                                <Link className={styles.a} href='/'>Home</Link>
-                            </li>
-                            <li className={styles.links}>
-                                <Link className={styles.a} href="/star_rail">Star Rail</Link>
-                            </li>
-                            <li>
-                                <Link className={styles.a} href="/reliquia">Relíquia</Link>
-                            </li>
-                        </ul>
-                    )}
-
-
+                    <Image className={styles.img} src="/images/pompom.png" alt="sim" width={60} height={60} />
+                    <li className={styles.links}>
+                        <Link className={styles.a} href='/'>Home</Link>
+                    </li>
+                    <li className={styles.links}>
+                        <Link className={styles.a} href="/star_rail">Star Rail</Link>
+                    </li>
+                    <li>
+                        <Link className={styles.a} href="/reliquia">Relíquia</Link>
+                    </li>
                 </ul>
-
             </nav>
         </header>
-    )
+    );
 }
